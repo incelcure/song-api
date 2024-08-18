3731c4be35eef576792bfc8d8c76b4f238883b88
harpoon <sergeekoilia99@gmail.com>
2024-08-18 22:46:12 +0300

    feat: file service logic

---
+++ file-api/src/main/scala/FileSvc.scala
@@ -0,0 +1,7 @@
+import java.io.File
+import scala.util.Try
+
+trait FileSvc{
+  def upload(file: Array[Byte], filename: String): Try[String]
+  def download(filename: String): Try[Array[Byte]]
+}
---
+++ file-api/src/main/scala/SystemFileSvc.scala
@@ -0,0 +1,19 @@
+import java.io.{File, FileInputStream, FileOutputStream}
+import scala.util
+import scala.util.{Try, Using}
+
+class SystemFileSvc extends FileSvc {
+
+  override def upload(file: Array[Byte], filename: String): Try[String] = Try {
+    Using.resource(new FileOutputStream(filename)) { stream =>
+      stream.write(file)
+      filename
+    }
+  }
+
+  override def download(filename: String): Try[Array[Byte]] = Try {
+    Using.resource(new FileInputStream(filename)) { stream =>
+      stream.readAllBytes()
+    }
+  }
+}
