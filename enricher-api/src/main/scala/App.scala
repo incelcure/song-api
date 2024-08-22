dc7e4406ab583de8bd39e62d2475a74509a130d8
harpoon <sergeekoilia99@gmail.com>
2024-08-22 16:03:47 +0300

    feat: receiving access token from spotify api

--- enricher-api/src/main/scala/App.scala
+++ enricher-api/src/main/scala/App.scala
@@ -7,5 +7,18 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Hey, enricher API")
 
+
+    val back = HttpClientSyncBackend()
+    val clientId = System.getenv("CLIENT_ID")
+    val clientSecret = System.getenv("CLIENT_SECRET")
+    val tokenRequest = basicRequest
+      .post(uri"https://accounts.spotify.com/api/token")
+      .auth
+      .basic(clientId, clientSecret)
+      .body(Map("grant_type" -> "client_credentials"))
+
+    val tokenResponse = tokenRequest.response(asStringAlways).send(back)
+    val access_token = tokenResponse.body
+    println(access_token)
   }
 }
