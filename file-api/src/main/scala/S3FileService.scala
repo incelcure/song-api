2f4b06b9cb0fa52b106176af9e8b1ab9aa1a106a
harpoon <sergeekoilia99@gmail.com>
2024-08-25 13:24:04 +0300

    feat: s3 donwload

--- file-api/src/main/scala/S3FileService.scala
+++ file-api/src/main/scala/S3FileService.scala
@@ -1,16 +1,16 @@
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.util.IOUtils
 
 import java.io.{File, FileInputStream, FileOutputStream}
 import scala.util.{Try, Using}
 
 class S3FileService extends FileService {
+  val awsCredentials = new BasicAWSCredentials(System.getenv("S3_ACCESS_KEY"), System.getenv("S3_SECRET_KEY"))
+  val amazonS3Client = new AmazonS3Client(awsCredentials)
+  val BUCKET_NAME = "song-bucket"
 
-  override def upload(file: Array[Byte], filename: String): Try[String] = Try{
-    val awsCredentials = new BasicAWSCredentials(System.getenv("S3_ACCESS_KEY"), System.getenv("S3_SECRET_KEY"))
-    val amazonS3Client = new AmazonS3Client(awsCredentials)
-
-    val BUCKET_NAME = "song-api-bucket"
+  override def upload(file: Array[Byte], filename: String): Try[String] = Try {
 
     amazonS3Client.createBucket(BUCKET_NAME)
     val s3File = new File(filename)
@@ -19,5 +19,9 @@ class S3FileService extends FileService {
     ""
   }
 
-  override def download(filename: String): Try[Array[Byte]] = ???
+  override def download(filename: String): Try[Array[Byte]] = Try {
+    val obj = amazonS3Client.getObject(BUCKET_NAME, filename)
+    IOUtils.toByteArray(obj.getObjectContent())
+
+  }
 }
