cf017727392fff854c16f0590762353a2a24acde
harpoon <sergeekoilia99@gmail.com>
2024-08-25 15:56:44 +0300

    feat: Enricher service basic setup

--- enricher-api/src/main/scala/App.scala
+++ enricher-api/src/main/scala/App.scala
@@ -9,35 +9,5 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Hey, enricher API")
 
-
-    val back = HttpClientSyncBackend()
-    val clientId = System.getenv("CLIENT_ID")
-    val clientSecret = System.getenv("CLIENT_SECRET")
-    val tokenRequest = basicRequest
-      .post(uri"https://accounts.spotify.com/api/token")
-      .auth
-      .basic(clientId, clientSecret)
-      .body(Map("grant_type" -> "client_credentials"))
-
-
-    val clientCredentials = tokenRequest.response(asStringAlways).send(back).body
-    val json = parse(clientCredentials).getOrElse(Json.Null)
-    val accessToken = json.hcursor.downField("access_token").as[String].getOrElse("")
-
-    println(accessToken)
-
-    val songId = "64xpre2xJX11xbKq4wWdNH" // Avenade - Just smile and wave boys
-
-    val metaSongRequest = basicRequest
-      .get(uri"https://api.spotify.com/v1/tracks/$songId")
-      .auth
-      .bearer(accessToken)
-      .response(asStringAlways)
-      .send(back)
-      .body
-
-    val songMeta = parse(metaSongRequest).getOrElse(Json.Null)
-    println(songMeta)
-
   }
 }
--- enricher-api/src/main/scala/Enricher.scala
+++ enricher-api/src/main/scala/Enricher.scala
@@ -1,3 +1,6 @@
+import io.circe.Json
+import io.circe.parser.parse
+
 import scala.util.Try
 import scala.Predef
 import sttp.client4.httpclient.HttpClientSyncBackend
@@ -5,6 +8,30 @@ import sttp.client4._
 
 
 class Enricher {
+  private val back: WebSocketSyncBackend = HttpClientSyncBackend()
+  private val clientId = System.getenv("CLIENT_ID")
+  private val clientSecret = System.getenv("CLIENT_SECRET")
+  private val tokenRequest: Request[Either[String, String]] = basicRequest
+    .post(uri"https://accounts.spotify.com/api/token")
+    .auth
+    .basic(clientId, clientSecret)
+    .body(Map("grant_type" -> "client_credentials"))
 
+
+  private val clientCredentials = tokenRequest.response(asStringAlways).send(back).body
+  private val json = parse(clientCredentials).getOrElse(Json.Null)
+  private val accessToken = json.hcursor.downField("access_token").as[String].getOrElse("")
+
+  def getMeta(songId: String): Json = {
+    val metaSongRequest = basicRequest
+      .get(uri"https://api.spotify.com/v1/tracks/$songId")
+      .auth
+      .bearer(accessToken)
+      .response(asStringAlways)
+      .send(back)
+      .body
+
+    parse(metaSongRequest).getOrElse(Json.Null)
+  }
 }
 
