5627455d15d345caee5d46d3d88be7615f513e42
harpoon <sergeekoilia99@gmail.com>
2024-08-27 17:57:50 +0300

    feat: firest doobie query to local pg

--- song-api/build.sbt
+++ song-api/build.sbt
@@ -3,10 +3,12 @@ lazy val `song-api` = (project in file("."))
     name := "song-api",
   )
 
+lazy val doobieVersion = "1.0.0-RC4"
+
 libraryDependencies ++= Seq(
-  "org.tpolecat" %% "doobie-core" % "1.0.0-RC2",
-  "org.tpolecat" %% "doobie-hikari" % "1.0.0-RC2",
-  "org.tpolecat" %% "doobie-postgres" % "1.0.0-RC2",
-  "org.tpolecat" %% "doobie-specs2" % "1.0.0-RC2" % "test",
-  "org.tpolecat" %% "doobie-scalatest" % "1.0.0-RC2" % "test"
+  "org.tpolecat" %% "doobie-core"     % doobieVersion,
+  "org.tpolecat" %% "doobie-postgres" % doobieVersion,
+  "org.tpolecat" %% "doobie-specs2"   % doobieVersion,
+  "org.typelevel" %% "cats-effect" % "3.5.4",
+  "org.postgresql" % "postgresql" % "42.7.3"
 )
--- song-api/src/main/scala/App.scala
+++ song-api/src/main/scala/App.scala
@@ -1,6 +1,31 @@
+import doobie._
+import doobie.implicits._
+import cats._
+import cats.effect._
+import cats.effect.unsafe.implicits.global
+import cats.implicits._
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hey, Song Api")
+
+    val pgUrl = System.getenv("POSTGRES_DB_URL")
+    val pgUser = System.getenv("POSTGRES_USER")
+    val pgPassword = System.getenv("POSGRES_PASSWORD")
+
+    val xa = Transactor.fromDriverManager[IO](
+      driver = "org.postgresql.Driver",
+      url = s"jdbc:postgresql://$pgUrl",
+      user = pgUser,
+      password = pgPassword,
+      logHandler = None
+    )
+    val metaRequest = sql"select meta_data from song_meta"
+      .query[String]
+      .to[List]
+
+    val ioMetaRequest = metaRequest.transact(xa)
+    println(ioMetaRequest.unsafeRunSync())
 
   }
 }
