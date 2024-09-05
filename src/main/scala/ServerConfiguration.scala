26db1bef13aae7d5bc6814e650aa92416064076a
Ilya Sergeenko <sergeekoilia99@gmail.com>
2024-09-05 11:35:55 +0300

    feat: base64 authorization decoded

--- src/main/scala/ServerConfiguration.scala
+++ src/main/scala/ServerConfiguration.scala
@@ -15,10 +15,10 @@ import sttp.capabilities.akka.AkkaStreams
 
 import java.io.File
 import java.nio.file.Files
+import java.util.Base64
 import scala.io.StdIn
 import sttp.tapir.json.circe._
 import sttp.tapir.server.ServerEndpoint //implicits for decode json
-
 
 case class MultipartFileData(filename: String, file: Part[File])
 
@@ -29,10 +29,10 @@ class ServerConfiguration {
   implicit val materializer: Materializer = Materializer(actorSystem)
   implicit val executionContext: ExecutionContextExecutor = actorSystem.dispatcher
   val s3Client = new S3FileService
-  val enricher = new EnricherService
+//  val enricher = new EnricherService
   val dbService = new SongDBService
 
-  val uploadEndpoint : ServerEndpoint[WebSockets with AkkaStreams, Future] = endpoint
+  val uploadEndpoint: ServerEndpoint[WebSockets with AkkaStreams, Future] = endpoint
     .summary("upload file to S3")
     .in("upload")
     .in(multipartBody[MultipartFileData])
@@ -49,31 +49,55 @@ class ServerConfiguration {
 
   val uploadRoute = AkkaHttpServerInterpreter().toRoute(uploadEndpoint)
 
+  //  val downloadEndpoint = endpoint
+  //    .summary("download file from s3")
+  //    .in("download" / query[String]("filename"))
+  //    .in(header[String]("Authorization"))
+  //    .get
+  //    .out(jsonBody[Array[Byte]])
+  //    .serverLogicSuccess {
+  //      filename =>
+  //        println(filename._1)
+  //        println(filename._2)
+  //      //        Right(s3Client.download(filename).get)
+  //    }
+
   val downloadEndpoint = endpoint
     .summary("download file from s3")
     .in("download" / query[String]("filename"))
+    .in(header[String]("Authorization"))
     .get
-    .out(jsonBody[Array[Byte]])
+    .out(jsonBody[Int])
+    .serverLogicSuccess[Future] {
+      request_data => Future {
+        val filename = request_data._1
+        val authBase = request_data._2
+        val encodedCredentials = Base64.getDecoder.decode(authBase.split(" ").last).map(_.toChar).mkString
+        println(filename)
+        println(encodedCredentials)
+        1
+      }
+      //        Right(s3Client.download(filename).get)
+    }
 
-  val downloadRoute = AkkaHttpServerInterpreter().toRoute(downloadEndpoint.serverLogicPure[Future] { filename =>
-    Right(s3Client.download(filename).get)
-  })
+  val downloadRoute = AkkaHttpServerInterpreter().toRoute(downloadEndpoint)
 
 
-  val downloadWithMetaEndpoint = endpoint
-    .summary("download file from s3 with meta info")
-    .in("download-with-meta" / query[String]("file-id"))
-    .get
-    .out(multipartBody[MultipartFileWithMeta])
+//  val downloadWithMetaEndpoint = endpoint
+//    .summary("download file from s3 with meta info")
+//    .in("download-with-meta" / query[String]("file-id"))
+//    .get
+//    .out(multipartBody[MultipartFileWithMeta])
+//
+//  val downloadWithMetaRoute = AkkaHttpServerInterpreter()
+//    .toRoute(downloadWithMetaEndpoint.serverLogicPure[Future] { fileId =>
+//      val file = s3Client.download(fileId)
+//      val meta = enricher.getMeta(fileId)
+//      Right(MultipartFileWithMeta(meta, file.get))
+//    })
 
-  val downloadWithMetaRoute = AkkaHttpServerInterpreter()
-    .toRoute(downloadWithMetaEndpoint.serverLogicPure[Future] { fileId =>
-      val file = s3Client.download(fileId)
-      val meta = enricher.getMeta(fileId)
-      Right(MultipartFileWithMeta(meta, file.get))
-    })
-
-  val routes = uploadRoute ~ downloadRoute ~ downloadWithMetaRoute
+//  val routes = uploadRoute ~ downloadRoute ~ downloadWithMetaRoute
+val routes = uploadRoute ~ downloadRoute
 
   //    val bindingFuture: Future[Http.ServerBinding] = Http().bindAndHandle(routes, "localhost", 8080)
   val bindFuture: Future[Http.ServerBinding] = Http().newServerAt("localhost", 8080).bind(routes)
