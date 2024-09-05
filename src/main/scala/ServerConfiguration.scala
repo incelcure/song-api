b3c855b7b2b4d3cf0cc90d974d02e542c32f8ea1
Ilya Sergeenko <sergeekoilia99@gmail.com>
2024-09-05 12:41:53 +0300

    feat: made auth request!

--- src/main/scala/ServerConfiguration.scala
+++ src/main/scala/ServerConfiguration.scala
@@ -2,6 +2,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.stream.Materializer
+
 import sttp.model.{Part, StatusCode}
 import sttp.tapir._
 import sttp.tapir.server.akkahttp.AkkaHttpServerInterpreter
@@ -9,9 +10,12 @@ import sttp.tapir.generic.auto._
 import sttp.tapir.json.circe.jsonBody
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
-import io.circe.Json
+import io.circe.{Decoder, Encoder, Json}
 import sttp.capabilities.WebSockets
 import sttp.capabilities.akka.AkkaStreams
+import sttp.client4.httpclient.HttpClientSyncBackend
+import sttp.client4.{Request, UriContext, WebSocketSyncBackend, asStringAlways, basicRequest, multipart}
+import sttp.client4._
 
 import java.io.File
 import java.nio.file.Files
@@ -29,7 +33,7 @@ class ServerConfiguration {
   implicit val materializer: Materializer = Materializer(actorSystem)
   implicit val executionContext: ExecutionContextExecutor = actorSystem.dispatcher
   val s3Client = new S3FileService
-//  val enricher = new EnricherService
+  val enricher = new EnricherService
   val dbService = new SongDBService
 
   val uploadEndpoint: ServerEndpoint[WebSockets with AkkaStreams, Future] = endpoint
@@ -69,35 +73,49 @@ class ServerConfiguration {
     .get
     .out(jsonBody[Int])
     .serverLogicSuccess[Future] {
-      request_data => Future {
-        val filename = request_data._1
-        val authBase = request_data._2
-        val encodedCredentials = Base64.getDecoder.decode(authBase.split(" ").last).map(_.toChar).mkString
-        println(filename)
-        println(encodedCredentials)
-        1
-      }
+      request_data =>
+        Future {
+          val filename = request_data._1
+          val authBase = request_data._2
+          val encodedCredentials = Base64.getDecoder.decode(authBase.split(" ").last).map(_.toChar).mkString
+          val creds = encodedCredentials.split(":")
+
+          val back: WebSocketSyncBackend = HttpClientSyncBackend()
+
+          val tokenRequest: Request[Either[String, String]] = basicRequest
+            .get(uri"http://127.0.0.1:8081/login")
+            .multipartBody(multipart("name", creds(0)), multipart("password", creds(1)))
+
+          val isValid = tokenRequest.response(asStringAlways)
+            .send(back)
+            .body
+            .toBoolean
+          println(s"is Valid $isValid")
+          println(filename)
+          println(encodedCredentials)
+          1
+        }
       //        Right(s3Client.download(filename).get)
     }
 
   val downloadRoute = AkkaHttpServerInterpreter().toRoute(downloadEndpoint)
 
 
-//  val downloadWithMetaEndpoint = endpoint
-//    .summary("download file from s3 with meta info")
-//    .in("download-with-meta" / query[String]("file-id"))
-//    .get
-//    .out(multipartBody[MultipartFileWithMeta])
-//
-//  val downloadWithMetaRoute = AkkaHttpServerInterpreter()
-//    .toRoute(downloadWithMetaEndpoint.serverLogicPure[Future] { fileId =>
-//      val file = s3Client.download(fileId)
-//      val meta = enricher.getMeta(fileId)
-//      Right(MultipartFileWithMeta(meta, file.get))
-//    })
+  //  val downloadWithMetaEndpoint = endpoint
+  //    .summary("download file from s3 with meta info")
+  //    .in("download-with-meta" / query[String]("file-id"))
+  //    .get
+  //    .out(multipartBody[MultipartFileWithMeta])
+  //
+  //  val downloadWithMetaRoute = AkkaHttpServerInterpreter()
+  //    .toRoute(downloadWithMetaEndpoint.serverLogicPure[Future] { fileId =>
+  //      val file = s3Client.download(fileId)
+  //      val meta = enricher.getMeta(fileId)
+  //      Right(MultipartFileWithMeta(meta, file.get))
+  //    })
 
-//  val routes = uploadRoute ~ downloadRoute ~ downloadWithMetaRoute
-val routes = uploadRoute ~ downloadRoute
+  //  val routes = uploadRoute ~ downloadRoute ~ downloadWithMetaRoute
+  val routes = uploadRoute ~ downloadRoute
 
   //    val bindingFuture: Future[Http.ServerBinding] = Http().bindAndHandle(routes, "localhost", 8080)
   val bindFuture: Future[Http.ServerBinding] = Http().newServerAt("localhost", 8080).bind(routes)
@@ -109,4 +127,3 @@ val routes = uploadRoute ~ downloadRoute
       .onComplete(_ => actorSystem.terminate())
   }
 }
-
