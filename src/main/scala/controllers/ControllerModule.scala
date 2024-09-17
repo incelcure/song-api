a879fa166df04c02003f8b64bd85d3e636188dbe
harpoon <sergeekoilia99@gmail.com>
2024-09-17 21:17:25 +0300

    feat: service and controller modules

--- src/main/scala/controllers/ControllerModule.scala
+++ src/main/scala/controllers/ControllerModule.scala
@@ -1,5 +1,14 @@
 package controllers
 
-class ControllerModule {
+import auth.AuthEndpointBuilder
+import services.ServiceModule
+
+import scala.concurrent.ExecutionContext
+
+class ControllerModule(serviceModule: ServiceModule)(implicit ex: ExecutionContext) {
+  val authEndpointBuilder = new AuthEndpointBuilder(serviceModule.authClient)
+
+  val fileController =
+    new FileController(serviceModule.s3FileService, authEndpointBuilder)(serviceModule.enricherService)
 
 }
--- src/main/scala/services/ServiceModule.scala
+++ src/main/scala/services/ServiceModule.scala
@@ -1,5 +1,15 @@
 package services
 
-class ServiceModule{
+import com.amazonaws.services.s3.AmazonS3
+import _root_.{EnricherService, S3FileService, SongDBService}
+import auth.{AuthClient, AuthClientImpl}
+import sttp.client4.WebSocketSyncBackend
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class ServiceModule(s3Client: AmazonS3, s3BucketName: String, httpClient: WebSocketSyncBackend) {
+  val s3FileService = new S3FileService(s3Client, s3BucketName)
+  val enricherService: EnricherService = new EnricherService
+  val songDBService: SongDBService = new SongDBService
+  val authClient: AuthClient = new AuthClientImpl(httpClient)
 }
