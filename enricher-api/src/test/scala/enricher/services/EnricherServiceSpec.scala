5e070039f7e9aeee5c6096626d6cdd1239894bc7
harpoon <sergeekoilia99@gmail.com>
2024-10-09 19:31:06 +0300

    feat: enricher specs

--- enricher-api/src/test/scala/enricher/services/EnricherServiceSpec.scala
+++ enricher-api/src/test/scala/enricher/services/EnricherServiceSpec.scala
@@ -1,20 +1,25 @@
 package enricher.services
 
 import io.circe._
+import io.circe.parser._
+import io.circe.JsonObject
+import io.circe.syntax.EncoderOps
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import sttp.client4.WebSocketSyncBackend
+import sttp.client4.httpclient.HttpClientSyncBackend
 
 class EnricherServiceSpec extends AnyFunSuite with Matchers {
-//  test("artist name should be Avenade"){
-//    val enricher = new EnricherService
-//    val artistNames: Option[List[String]] = for {
-//      artistsJson <-  enricher
-//        .getMeta("64xpre2xJX11xbKq4wWdNH") // Avenade - Just smile and wave boys
-//        .hcursor
-//        .downField("artists").as[List[Json]].toOption
-//      names = artistsJson.flatMap(_.hcursor.downField("name").as[String].toOption)
-//    } yield names
-//
-//    artistNames.get.head shouldBe "Avenade"
-//  }
+  val back: WebSocketSyncBackend = HttpClientSyncBackend()
+  val enricher = new EnricherService(back)
+
+  test("artist name should be Avenade"){
+    val artistNames: Option[List[String]] =
+      for {
+        artistsJson <- enricher.getMeta("64xpre2xJX11xbKq4wWdNH").get.hcursor.downField("artists").as[List[Json]].toOption
+        names = artistsJson.flatMap(_.hcursor.downField("name").as[String].toOption)
+      } yield names
+
+    artistNames.get.head shouldBe "Avenade"
+  }
 }
